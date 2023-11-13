18782610b75d770b023e129cec22710c1ea7e33a
bisds <ratefiarison25@gmail.com>
2023-11-13 17:18:44 +0200

    modif

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StartApplication {
 
+
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "TELMA MADAGASCAR");
