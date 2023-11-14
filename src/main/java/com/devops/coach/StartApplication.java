c136c03895b78089266c30d3b397a3976ea95fa4
bisds <ratefiarison25@gmail.com>
2023-11-14 21:24:49 +0300

    Modif

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -13,8 +13,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "TELMA");
-        model.addAttribute("msg","FIXE-MOBILE-INTERNET");
+        model.addAttribute("title", "BONJOUR");
+        model.addAttribute("msg","BIENVENUE A MA PRESENTATION");
         return "index";
     }
 
