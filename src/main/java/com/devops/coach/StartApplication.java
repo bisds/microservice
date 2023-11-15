3a008fc6b84eb36faf9872045b670b9149a1985c
bisds <ratefiarison25@gmail.com>
2023-11-15 06:28:31 +0300

    modif

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -13,8 +13,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "TELMA");
-        model.addAttribute("msg","la numéro 1");
+        model.addAttribute("title", "TELMA Madagascar");
+        model.addAttribute("msg","FIXE MOBILE INTERNET");
         return "index";
     }
 
